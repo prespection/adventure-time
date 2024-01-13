55ce7c3a9a1b265c4f4caae07515afdc3b55a487
prespection <drftmodz@yahoo.com>
2024-01-13 13:17:23 -0500

    First Game Commit

---
+++ MainGame.py
@@ -0,0 +1,17 @@
+print("Hello, welcome to Adventure Time! \nWhat is your going to be your Adventurer's Name?")
+Username = input("Enter username: ")
+print ("Your Adventurer's Name is: " + Username)
+print ("Hello " + Username + ", I'm so glad that you're here! There is a dragon wrecking havoc on the city... Can you please us kill it?")
+KillorNot = input ("Do you want to help your city's citizens kill the dragon? Y/N")
+
+if KillorNot == "Y":
+    print ("Oh thank you so much! The dragon is hiding in a secret lair under the town hall. You'll need some equipment first before you can take it on")
+elif KillorNot == "N":
+    print ("Please help us kill the dragon, we really need you out there! We will compensate you greatly")
+KillorNot = input ("Do you want to help your city's citizens kill the dragon? Y/N ")
+
+while KillorNot == "N":
+    print ("Please help us kill the dragon, we really need you out there! We will compensate you greatly")
+    KillorNot = input ("Do you want to help your city's citizens kill the dragon? Y/N ")
+    if KillorNot == "Y":
+            print ("Oh thank you so much! The dragon is hiding in a secret lair under the town hall. You'll need some equipment first before you can take it on")
