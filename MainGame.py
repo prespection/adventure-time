f1eb33203d7f9d0bd654d777e445349837b8778e
prespection <drftmodz@yahoo.com>
2024-01-21 17:11:44 -0500

    test

--- MainGame.py
+++ MainGame.py
@@ -1,21 +1,20 @@
 print("Hello, welcome to Adventure Time! \nWhat is your going to be your Adventurer's Name?")
 Username = input("Enter username: ")
-print ("Your Adventurer's Name is: " + Username)
-print ("Hello " + Username + ", I'm so glad that you're here! There is a dragon wrecking havoc on the city... Can you please us kill it?")
-KillorNot = input ("Do you want to help your city's citizens kill the dragon? Y/N")
+print("Your Adventurer's Name is: " + Username)
+print(
+    "Hello " + Username + ", I'm so glad that you're here! There is a dragon wrecking havoc on the city... Can you please us kill it?")
+KillorNot = input("Do you want to help your city's citizens kill the dragon? Y/N")
 
 if KillorNot == "Y":
-    print ("Oh thank you so much! The dragon is hiding in a secret lair under the town hall. You'll need some equipment first before you can take it on")
+    print(
+        "Oh thank you so much! The dragon is hiding in a secret lair under the town hall. You'll need some equipment first before you can take it on")
 else:
     while KillorNot != "Y":
-        print ("\nPlease help us kill the dragon, we really need you out there! We will compensate you greatly")
-        KillorNot = input ("Do you want to help your city's citizens kill the dragon? Y/N")
+        print("\nPlease help us kill the dragon, we really need you out there! We will compensate you greatly")
+        KillorNot = input("Do you want to help your city's citizens kill the dragon? Y/N")
         if KillorNot != "N" and KillorNot != "Y":
-            print ("Please enter a valid input.")
-             
-        
-print ("Oh thank you so much! The dragon is hiding in a secret lair under the town hall. You'll need some equipment first before you can take it on")
+            print("Please enter a valid input.")
 
-# elif KillorNot == "N":
-#     print ("Please help us kill the dragon, we really need you out there! We will compensate you greatly")
-#     KillorNot = input ("Do you want to help your city's citizens kill the dragon? Y/N ")
+print(
+    "Oh thank you so much! The dragon is hiding in a secret lair under the town hall. You'll need some equipment first before you can take it on")
+# for loop, try to use dictionary, try to use array (if applicable)
